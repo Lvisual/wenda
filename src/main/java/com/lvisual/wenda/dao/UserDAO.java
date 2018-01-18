c9c3a0a947edbf07e28c2a233cae1eeec8851fd6
Lvisual <873584843@qq.com>
2018-01-18 18:57:00 +0800

    second

--- src/main/java/com/lvisual/wenda/dao/UserDAO.java
+++ src/main/java/com/lvisual/wenda/dao/UserDAO.java
@@ -9,16 +9,16 @@ import javax.jws.soap.SOAPBinding;
 @Mapper
 @Component
 public interface UserDAO {
-   @Insert(("insert into user (name,password) values(#{name},#{password})"))
+   @Insert({("insert into user (name,password) values(#{name},#{password})")})
    int addUser(User user);
 
-   @Select("select name,password from user where id = #{id}")
+   @Select({"select name,password from user where id = #{id}"})
    User selectById(int id);
 
-   @Update("update user  set password = #{password} where id = #{id}")
+   @Update({"update user  set password = #{password} where id = #{id}"})
    void upDatePassword(User user);
 
-   @Delete("delete from user where id = #{id}")
+   @Delete({"delete from user where id = #{id}"})
    void deleteById(int id);
 
 }
